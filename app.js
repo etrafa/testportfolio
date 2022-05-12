051465588aa4a3c084d2996805f1177d167e87a7
Etem Senel <etemsenel96@hotmail.de>
2022-05-12 05:19:56 +0300

    first commit.

---
+++ app.js
@@ -0,0 +1,60 @@
+"using strict";
+
+// const controller = new ScrollMagic.Controller();
+
+// const timeline = new TimelineMax();
+
+// timeline
+//   .from(".developer-name", 4, {
+//     fontSize: "20px",
+//     ease: Power3.easeInOut,
+//   })
+//   .from(
+//     ".title",
+//     4,
+//     {
+//       fontSize: "5px",
+//       ease: Power3.easeInOut,
+//     },
+//     "-=4"
+//   )
+//   .from(
+//     ".balloon-first",
+//     4,
+//     {
+//       bottom: "80%",
+//       ease: Power1.easeInOut,
+//     },
+//     "-=4"
+//   );
+// const scene = new ScrollMagic.Scene({
+//   triggerElement: ".home",
+//   duration: "100%",
+//   triggerHook: 0,
+//   offset: "20",
+// })
+//   .setTween(timeline)
+//   .setPin(".home")
+//   .addTo(controller);
+
+// const homeSection = document.querySelector(".home");
+// const aboutSection = document.querySelector(".about");
+
+const keyCounter = 0;
+
+// document.addEventListener("keyup", (e) => {
+//   if (e.key === "ArrowDown") {
+//     homeSection.classList.add("home-active");
+//     console.log(e.key);
+//   } else if (e.key === "ArrowUp") {
+//     homeSection.classList.remove("home-active");
+//     console.log(e.key);
+//   }
+// });
+
+// const typed = new Typed(".developer-name", {
+//   strings: ["Etem Senel"],
+//   typeSpeed: 160,
+//   loop: false,
+//   showCursor: false,
+// });
